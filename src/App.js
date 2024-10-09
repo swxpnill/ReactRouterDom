31234c1bd75cf1fe68d43c42818d1a63e3eba86b
Swapnil <swapnilp161998@gmail.com>
2024-10-09 11:48:59 +0530

    React Router Dom

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-gray-700" p-4 >Learning React Router</h1>
+      {/* <Header />
+      <Footer />
+      <Home /> */}
+    </>
+  );
+}
+
+export default App;
